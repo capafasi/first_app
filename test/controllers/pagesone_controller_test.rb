1077b55e68cbf1ce937c163081709afcff55be94
Camila <cpfaundes@ug.uchile.cl>
2020-03-10 21:05:38 -0300

    cuarto commit

--- test/controllers/pagesone_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PagesoneControllerTest < ActionDispatch::IntegrationTest
-  test "should get two" do
-    get pagesone_two_url
-    assert_response :success
-  end
-
-end
